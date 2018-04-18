5bc12397962ad72a17a4ad0a4c2182d82b8d6ba4
ShinjaliShah912 <shinjali.shah@gmail.com>
2018-04-18 18:29:46 +0530

    Add files via upload

---
+++ spring-rest-hibernate/src/main/java/org/javasavvy/spring/services/CustomerService.java
@@ -0,0 +1,15 @@
+package org.javasavvy.spring.services;
+
+import java.util.Date;
+import java.util.List;
+
+import org.javasavvy.spring.entity.Customer;
+
+public interface CustomerService {
+	
+	public Customer addCustomer(Customer customer);
+	public Customer updateCustomer(Customer customer);
+	public Customer getCustomer(long customerId);
+	public List<Customer> getAllCustomers();
+	public  void deleteCustomer(long customerId);
+}
